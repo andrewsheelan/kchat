b69576e54e7e465c0a5027508ab9c6a05115b160
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2021-01-21 15:47:13 -0500

    Add initial pages

--- db/migrate/20151005124638_add_md5_to_user.rb
+++ db/migrate/20151005124638_add_md5_to_user.rb
@@ -1,4 +1,4 @@
-class AddMd5ToUser < ActiveRecord::Migration
+class AddMd5ToUser < ActiveRecord::Migration[4.2]
   def up
     add_column :users, :md5, :string
     User.all.each{|user| user.update(updated_at: Time.now) }
--- db/migrate/20151007032440_add_conversation_to_chats.rb
+++ db/migrate/20151007032440_add_conversation_to_chats.rb
@@ -1,4 +1,4 @@
-class AddConversationToChats < ActiveRecord::Migration
+class AddConversationToChats < ActiveRecord::Migration[4.2]
   def change
     add_column :chats, :conversation, :string
   end
