ba75becc1f6b854fee840ad2d1ca1bc9ec7d403e
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2021-01-21 19:58:47 -0500

    Handle exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  around_action :handle_exceptions
+
+# Catch exception and return JSON-formatted error
+  def handle_exceptions
+    begin
+      yield
+    rescue ActiveRecord::RecordNotFound => e
+      @status = 404
+      @message = 'Record not found'
+    rescue ActiveRecord::RecordInvalid => e
+      render_unprocessable_entity_response(e.record) && return
+    rescue ArgumentError => e
+      @status = 400
+    rescue StandardError => e
+      @status = 500
+    end
+    json_response({ success: false, message: @message || e.class.to_s, errors: [{ detail: e.message }] }, @status) unless e.class == NilClass
+  end
 end
